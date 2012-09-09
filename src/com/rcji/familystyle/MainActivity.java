da383a1f36c5d12ff505abe03631d86054bf342c
Ivan Thai <ithai89@berkeley.edu>
2012-09-09 00:45:52 -0700

    init commit

--- src/com/rcji/familystyle/MainActivity.java
+++ src/com/rcji/familystyle/MainActivity.java
@@ -1,8 +1,10 @@
 package com.rcji.familystyle;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
@@ -10,20 +12,49 @@ import android.widget.TextView;
 import com.rcji.familystyle.locu.Locu;
 
 public class MainActivity extends Activity {
-
+	TextView tv;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        TextView tv = (TextView) findViewById(R.id.maintv);
-        ArrayList<String> arr = Locu.query(41.214796,  -73.17417);
-        String tv_str = "";
-        for (String str: arr) {
-        	tv_str = tv_str + str + "\n";
-        }
-        tv.setText(tv_str);
+        tv = (TextView) findViewById(R.id.maintv);
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE); 
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //double longitude = location.getLongitude();
+      //  double latitude = location.getLatitude();
+        //lm.requestLocationUpdates(LocationManager.GPS, 2000, 10, locationListener);
+        
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+        
+        
     }
+    
+    private final LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            Locu.query(latitude,longitude, tv);
+        }
+
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+    };
+
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/rcji/familystyle/locu/Locu.java
+++ src/com/rcji/familystyle/locu/Locu.java
@@ -1,16 +1,21 @@
 package com.rcji.familystyle.locu;
 
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import android.os.Looper;
+import android.widget.TextView;
 
 import com.rcji.familystyle.JSON;
 
 public class Locu {
+	/*
 	public static ArrayList<String> query(double lat, double lng) {
 		String query = lat + "" + lng;
 		String line = "";
@@ -39,5 +44,89 @@ public class Locu {
 			
 		}
 		return arr;
+	} */
+	public static String API_KEY = "fed017c95941aecf151b324e8b61d286a6cda99d";
+	public static void query(double lat, double lng, final TextView tv) {
+		String query = lat+","+lng;
+	    String response = "";
+		final String URL = "http://api.locu.com/v1_0/venue/search/?category=restaurant&location="+query+"&radius=1600&api_key="+API_KEY;
+		//final String URL = "http://api.locu.com/v1_0/venue/search/?api_key=" + API_KEY;
+		
+
+		HttpClient client = new DefaultHttpClient();
+		Thread t = new Thread() {
+			public void run() {
+				Looper.prepare();
+				HttpClient client = new DefaultHttpClient();
+				//HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000);
+				HttpResponse resp;
+				ArrayList<String> arr = new ArrayList<String>();
+				//JSONObject jsonpost = new JSONObject();
+				//JSONArray jarray = new JSONArray();
+				try {
+					//Log.i("About to create a new http post");
+					//HttpGet get = new HttpGet("http://api.locu.com/v1_0/venue/search/?api_key=fed017c95941aecf151b324e8b61d286a6cda99d");
+					HttpGet get = new HttpGet(URL);
+					get.addHeader("accept", "application/json");
+					
+					//StringEntity se = new StringEntity(jsonpost.toString());
+					//se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+					//post.setEntity(se);
+					//Log.i("About to execute httppost");
+					resp = client.execute(get);
+					//Log.i("HTTPResponse: " + resp);
+					JSONObject responseObject = JSON.getJSONObject(resp.getEntity().getContent());
+					//Log.i("Net Call Success");
+					JSONArray objects = (JSONArray) responseObject.get("objects");
+					//String restaurantNames = "";
+					
+					//while ()
+					for (int i = 0; i<objects.length(); i ++){
+						arr.add(objects.getJSONObject(i).getString("name"));
+					}
+					
+					final ArrayList<String> arr1 = arr;
+					tv.post(new Runnable() {
+						
+						public void run() {
+							String tv_str = "";
+					        for (String str: arr1) {
+					        	tv_str = tv_str + str + "\n";
+					        }
+							tv.setText(tv_str);
+							
+						}
+					});
+					
+					//callback.onSuccess(responseObject);
+				} catch(Exception e) {
+					e.printStackTrace();
+					//callback.onFailure(e.getMessage());
+				}
+				Looper.loop();
+			}
+		};
+		t.start();
+/*
+		   try {
+		       client.executeMethod(method);
+		       if (method.getStatusCode() == HttpStatus.SC_OK) {
+		           response = method.getResponseBodyAsString();
+		           System.out.println("Response = " + response);
+		       }
+		   } catch (IOException e) {
+		       e.printStackTrace();
+		   } finally {
+		       method.releaseConnection();
+		   }
+*/
+		   //PARSE "response" INTO JSON OBJECT
+
+		   //INSERT JSON MANIPULATION TO GAIN NAME (possibly also lat/long of restaurant)
+	   
+
+	
 	}
+
+	
 }
