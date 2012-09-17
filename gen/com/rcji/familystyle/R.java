438a31842bf45564b411bc61acfff12267c8a99a
Ivan Thai <ithai89@berkeley.edu>
2012-09-16 23:19:16 -0700

    final commit

--- gen/com/rcji/familystyle/R.java
+++ gen/com/rcji/familystyle/R.java
@@ -11,21 +11,22 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int familystyleplatedown=0x7f020000;
-        public static final int familystyleplateup=0x7f020001;
-        public static final int ic_action_search=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int nom_csl=0x7f020004;
+        public static final int familystylelogo=0x7f020000;
+        public static final int familystylelogob=0x7f020001;
+        public static final int familystyleplatedown=0x7f020002;
+        public static final int familystyleplateup=0x7f020003;
+        public static final int ic_action_search=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int nom_csl=0x7f020006;
     }
     public static final class id {
         public static final int choicemain=0x7f070000;
         public static final int choicetv=0x7f070001;
         public static final int choicevenue=0x7f070002;
-        public static final int mainiv=0x7f070006;
-        public static final int maintv=0x7f070005;
-        public static final int maps_location_name=0x7f070004;
-        public static final int mapview=0x7f070003;
-        public static final int menu_settings=0x7f070007;
+        public static final int imageView1=0x7f070004;
+        public static final int mainiv=0x7f070005;
+        public static final int maintv=0x7f070003;
+        public static final int menu_settings=0x7f070006;
     }
     public static final class layout {
         public static final int activity_choice=0x7f030000;
@@ -42,5 +43,6 @@ public final class R {
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
+        public static final int NoActionBar=0x7f050001;
     }
 }
